0c2eb19274c25a39057df79552656aad0b9879e6
Shev <125613450+FrancescoTalento@users.noreply.github.com>
2025-09-20 22:42:39 -0300

    Initial Files of practicing Styled Components

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import styled from 'styled-components'
+import './App.css'
+
+const FundoGradinet = styled.div`
+  background: linear-gradient(174.61deg, #041833 4.16%, #04244F 48%, #154580 96.76%);
+  min-height: 100vh;
+  width: 100%;
+`;
+
+function App() {
+  
+
+  return (
+    <FundoGradinet>
+      
+    </FundoGradinet>
+  )
+}
+
+export default App
